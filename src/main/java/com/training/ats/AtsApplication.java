3dad0e9e6f1b581cfc175aea5a528d461cf02caa
aliahmed58 <aliahmednadeem5859@gmail.com>
2024-01-24 00:38:12 +0500

    init git, add datasource

---
+++ src/main/java/com/training/ats/AtsApplication.java
@@ -0,0 +1,13 @@
+package com.training.ats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsApplication.class, args);
+	}
+
+}
